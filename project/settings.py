7ee50fc82577b84fb07d00fa3515b0877fd0118d
mohamed <80061689+mohamed99elsokary@users.noreply.github.com>
2021-11-18 18:29:01 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -127,8 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/var/www/hd/static"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
