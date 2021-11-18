2ea2be3cea9a4698ad07ac745de54f2597071a7f
mohamed <80061689+mohamed99elsokary@users.noreply.github.com>
2021-11-18 17:50:50 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -128,10 +128,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = "/var/www/hd/static"
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
